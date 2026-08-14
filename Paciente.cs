ddee65f291e479716122ea0249f1a764f04675ae
Alejandra <153337125+alejandravaldz@users.noreply.github.com>
2026-08-13 23:06:12 -0400

    Paciente.cs

--- Paciente.cs
+++ Paciente.cs
@@ -1,25 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Sistema_de_Gestión_de_Pacientes
 {
-
-
-   
-
     public class Paciente
     {
-        
+        public enum sexo
+        {
+            Masculino,
+            Femenino
+        }
+        public enum Estadop
+        {
+            Ingresado,
+            EnObservacion,
+            DeAlta,
+            Hospitalizado
+
+        }
+      
             public string Cedula { get; set; }
             public string Nombre { get; set; }
             public int Edad { get; set; }
-            public string Estado { get; set; }
-        public string Sexo { get; set; }
+            public sexo Sexo { get; set; }
+            public Estadop Estado { get; set; }
+
             public string Diagnostico { get; set; }
-            public DateTime Fecha { get; set; }
+            public DateTime FechaIngreso { get; set; }
+            
 
 
-        
+
+        }
     }
-}
+
+
